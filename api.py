eec377394a0e13c3a50b130abc78c361757071d1
Taerghods <sanazzt88@gmail.com>
2025-08-30 14:04:53 +0330

    add: get & post In Api

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ import json
 import urllib.request
 import ssl
 
+""" GET """
 context = ssl._create_unverified_context()
 
 response_1 = urllib.request.urlopen(url='https://swapi.dev/api/starships/9/', context=context)
@@ -12,3 +13,14 @@ text_2 = response_2.read()
 
 print(json.loads(text_1.decode('utf-8')))
 print(json.loads(text_2.decode('utf-8')))
+
+
+""" POST """
+req = urllib.request.Request('https://httpbin.org/post', data=json.dumps({"name": "Sanaz Taerghods"}).encode())
+req.add_header('Content-Type', 'application/json')
+response_3 = urllib.request.urlopen(req)
+
+text_3 = response_3.read()
+
+print(json.loads(text_3.decode('utf-8')))
+
